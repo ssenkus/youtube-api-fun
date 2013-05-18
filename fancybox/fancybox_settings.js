410eff7624ed335c3af650e9779b6fa7bf99987f
Steven Senkus <steven.senkus@yahoo.com>
2013-05-17 20:23:23 -0700

    Good place to stop for now...

--- fancybox/fancybox_settings.js
+++ fancybox/fancybox_settings.js
@@ -9,12 +9,22 @@
 					prevEffect: 'fade',
 					padding: 15,
 					margin: 20, // Increase left/right margin
-					    helpers:  {
-        thumbs : {
-            width: 50,
-            height: 50
-        }
-    }
+					helpers:  {
+						thumbs: {
+							width: 50,
+							height: 50
+						},
+						media: {},
+						title: {
+							type: 'inside' 
+						},
+						overlay: {
+								closeClick : true,  // if true, fancyBox will be closed when user clicks on the overlay
+								speedOut   : 1000,   // duration of fadeOut animation
+								showEarly  : false,  // indicates if should be opened immediately or wait until the content is ready
+								locked     : true   // if true, the content will be locked into overlay
+							},
+					}
 				});
 
 		});
